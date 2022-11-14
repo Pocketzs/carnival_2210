65ef30a7f2301c5fafc0cd74d169f607a4ad3fb4
Nigel Aung-Myint <nigel.aungmyint@gmail.com>
2022-11-14 10:52:28 -0600

    Add ride rider log and board rider tests

--- spec/ride_spec.rb
+++ spec/ride_spec.rb
@@ -14,4 +14,30 @@ describe Ride do
       expect(ride1.total_revenue).to eq 0
     end
   end
+
+  describe '#rider_log' do
+    it 'starts as an empty hash for visitors and how many times they rode' do
+      ride1 = Ride.new({ name: 'Carousel', min_height: 24, admission_fee: 1, excitement: :gentle })
+      
+      expect(ride1.rider_log).to eq({})
+    end
+  end
+
+  describe '#board_rider' do
+    it 'records visitors in the rider log and how many times they rode' do
+      ride1 = Ride.new({ name: 'Carousel', min_height: 24, admission_fee: 1, excitement: :gentle })
+      visitor1 = Visitor.new('Bruce', 54, '$10')
+      visitor2 = Visitor.new('Tucker', 36, '$5')
+      visitor1.add_preference(:gentle)
+      visitor2.add_preference(:gentle)
+
+      expect(ride1.rider_log).to eq({})
+      
+      ride1.board_rider(visitor1)
+      ride1.board_rider(visitor2)
+      ride1.board_rider(visitor1)
+
+      expect(ride1.rider_log).to eq({visitor1 => 2, visitor2 => 1})
+    end
+  end
 end
