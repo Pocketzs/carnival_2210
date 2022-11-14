383be7a87a3d08617a6355b40f849565054aa8fa
Nigel Aung-Myint <nigel.aungmyint@gmail.com>
2022-11-14 11:10:53 -0600

    Add ride board rider reduce spending money test

--- spec/ride_spec.rb
+++ spec/ride_spec.rb
@@ -32,12 +32,30 @@ describe Ride do
       visitor2.add_preference(:gentle)
 
       expect(ride1.rider_log).to eq({})
-      
+
       ride1.board_rider(visitor1)
       ride1.board_rider(visitor2)
       ride1.board_rider(visitor1)
 
       expect(ride1.rider_log).to eq({visitor1 => 2, visitor2 => 1})
+    end
+
+    it 'reduces the riders spending money by the admission fee' do
+      ride1 = Ride.new({ name: 'Carousel', min_height: 24, admission_fee: 1, excitement: :gentle })
+      visitor1 = Visitor.new('Bruce', 54, '$10')
+      visitor2 = Visitor.new('Tucker', 36, '$5')
+      visitor1.add_preference(:gentle)
+      visitor2.add_preference(:gentle)
+
+      expect(visitor1.spending_money).to eq 10
+      expect(visitor2.spending_money).to eq 5
+
+      ride1.board_rider(visitor1)
+      ride1.board_rider(visitor2)
+      ride1.board_rider(visitor1)
+
+      expect(visitor1.spending_money).to eq 8
+      expect(visitor2.spending_money).to eq 4
     end
   end
 end
