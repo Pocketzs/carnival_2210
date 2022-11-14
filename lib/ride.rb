426807cc7b0d834f3d20bc191e10402ceda1a57c
Nigel Aung-Myint <nigel.aungmyint@gmail.com>
2022-11-14 11:49:32 -0600

    Pass ride board rider tall enough test

--- lib/ride.rb
+++ lib/ride.rb
@@ -17,7 +17,7 @@ class Ride
   end
 
   def board_rider(visitor)
-    return unless visitor.preferences.include?(@excitement)
+    return unless (visitor.preferences.include?(@excitement) && visitor.tall_enough?(@min_height))
     @rider_log[visitor] += 1
     visitor.spending_money -= @admission_fee
     @total_revenue += @admission_fee
